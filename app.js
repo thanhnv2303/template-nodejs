4add72c43ca7aa111264c85051496e6d1a12cf90
Dinh-Hoang Nam <dinhhoangnam162793@gmail.com>
2020-12-06 20:44:01 +0700

    :hatching_chick: init commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+require("dotenv").config();
+
+const cors = require("cors");
+app.use(cors());
+
+const accRouter = require("./routes/acc/acc-router");
+app.use(accRouter);
+
+const makeRequest = require("./routes/make-request/make-request");
+app.use(makeRequest);
+
+const votingRouter = require("./routes/voting/voting-router");
+app.use(votingRouter);
+
+app.listen(8003, () => {
+  console.log("App listening on port 8003!");
+});
