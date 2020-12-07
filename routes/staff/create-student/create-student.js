adac2f3d6880ea012f8720c9907c0a086f290a83
Dinh-Hoang Nam <dinhhoangnam162793@gmail.com>
2020-12-07 21:54:57 +0700

    :heavy_plus_sign: upload student api

---
+++ routes/staff/create-student/create-student.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const router = express.Router();
+const multer = require("multer");
+const upload = multer();
+const readXlsxFile = require("read-excel-file/node");
+const { authen, author } = require("../../acc/protect-middleware");
+const connection = require("../../../db");
+// const generator = require("generate-password");
+// const bcrypt = require("bcryptjs");
+const { ROLE } = require("../../acc/ROLE");
+
+const bip39 = require("bip39");
+const HdKey = require("hdkey");
+
+const { Duplex } = require("stream");
+function bufferToStream(myBuuffer) {
+  let tmp = new Duplex();
+  tmp.push(myBuuffer);
+  tmp.push(null);
+  return tmp;
+}
+
+router.post("/create-student", authen, author(ROLE.STAFF), upload.single("excel-file"), async (req, res) => {
+  try {
+    const studentHistoryCol = (await connection).db().collection("StudentHistory");
+    const hdKey = HdKey.fromMasterSeed(bip39.mnemonicToSeedSync(bip39.generateMnemonic()));
+    readXlsxFile(bufferToStream(req.file.buffer)).then(async (rows) => {
+      // skip header
+      rows.shift();
+      // parse excel
+      const students = rows.map((row, index) => {
+        const path = "m/44'/0'/0'/0/" + index;
+        const newAccNode = hdKey.derive(path);
+
+        let student = {
+          studentId: row[0],
+          name: row[1],
+          birthday: row[2],
+          class: row[3],
+          publicKey: newAccNode.publicKey.toString("base64"),
+          privateKey: newAccNode.privateKey.toString("base64"),
+        };
+        return student;
+      });
+
+      const insertStudentHistoryResult = await studentHistoryCol.insertOne({ time: new Date().toISOString().split("T")[0], profiles: students });
+      res.json(insertStudentHistoryResult.ops[0]);
+    });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+router.get("/student-history", authen, author(ROLE.STAFF), async (req, res) => {
+  try {
+    const studentHistoryCol = (await connection).db().collection("StudentHistory");
+    const result = await studentHistoryCol.find({}).toArray();
+    res.json(result);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+module.exports = router;
