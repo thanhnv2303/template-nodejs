ee2f1b5f34b5d2691e0328445e30e2109ac3d8da
Dinh-Hoang Nam <dinhhoangnam162793@gmail.com>
2020-12-11 17:35:46 +0700

    :construction: /make-join-request: post to rest-api

--- routes/staff/make-request/make-request.js
+++ routes/staff/make-request/make-request.js
@@ -5,10 +5,11 @@ const upload = multer();
 const { authen, author } = require("../../acc/protect-middleware");
 const connection = require("../../../db");
 const { profileSchema } = require("./schema");
-const sawtoothCli = require("./make-req-cli");
+// const sawtoothCli = require("./make-req-cli");
 const { ROLE } = require("../../acc/ROLE");
 const PROFILE = "UniversityProfile";
 const ObjectID = require("mongodb").ObjectID;
+const axios = require("axios").default;
 
 router.get("/university-profile", authen, author(ROLE.STAFF), async (req, res) => {
   try {
@@ -24,22 +25,6 @@ router.get("/university-profile", authen, author(ROLE.STAFF), async (req, res) =
     res.status(500).json(err.toString());
   }
 });
-
-// not use any more, fetch profile then update voting state instead
-// router.get("/votes", authen, author(ROLE.STAFF), async (req, res) => {
-//   try {
-//     const col = (await connection).db().collection(PROFILE);
-//     const profile = await col.findOne({ uid: req.user.uid });
-//     if (profile.votes) {
-//       res.json(profile.votes);
-//     } else {
-//       res.json([]);
-//     }
-//   } catch (error) {
-//     console.log(error);
-//     res.status(500).json(error.toString());
-//   }
-// });
 
 router.post("/make-request", authen, author(ROLE.STAFF), async (req, res) => {
   try {
@@ -59,13 +44,13 @@ router.post("/make-request", authen, author(ROLE.STAFF), async (req, res) => {
     }
 
     // forward to sawtooth-cli to make tx
-    const makeTxOp = await sawtoothCli.makeJoinRequest(profile, req.body.privateKeyHex);
-    if (makeTxOp.ok) {
+    const response = await makeJoinRequest(req.body.privateKeyHex, profile);
+    if (response.ok) {
       await col.updateOne({ uid: req.user.uid }, { $set: { ...profile, state: "voting" } }, { upsert: true });
-      res.json({ ok: true });
+      res.json({ ok: true, txid: response.txid });
     } else {
       await col.updateOne({ uid: req.user.uid }, { $set: { ...profile, state: "fail" } }, { upsert: true });
-      res.json({ ok: false });
+      res.json({ ok: false, msg: response.msg });
     }
   } catch (err) {
     res.status(500).json(err.toString());
@@ -87,5 +72,11 @@ router.post("/change-avatar", authen, author(ROLE.STAFF), upload.single("avatar"
     res.status(500).json(error.toString());
   }
 });
+
+async function makeJoinRequest(privateKeyHex, profile) {
+  // const res = await axios.post("/create_institution", { privateKeyHex, profile });
+  // return res.data;
+  return { ok: true, txid: "202ed8d21c2a2e75cd288bb14e2e8a2d940c32cd933" };
+}
 
 module.exports = router;
