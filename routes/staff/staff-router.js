28dd636ae2558ffb3253f4b7f385145a881eebb0
DinhHoangNam <dinhhoangnam162793@gmail.com>
2021-03-01 14:00:11 +0700

    :pencil2: rename file & folder

--- routes/staff/staff-router.js
+++ routes/staff/staff-router.js
@@ -1,19 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-const makeRequestRouter = require("./make-request/make-request");
-router.use(makeRequestRouter);
+router.use(require("./make-request"));
+router.use(require("./voting"));
+router.use(require("./bureau-apis"));
+router.use(require("./teacher-apis"));
+router.use(require("./student-apis"));
 
-const votingRouter = require("./voting/voting-router");
-router.use(votingRouter);
-
-const createBureauRouter = require("./create-bureau/create-bureau");
-router.use(createBureauRouter);
-
-const createTeacherRouter = require("./create-teacher/create-teacher");
-router.use(createTeacherRouter);
-
-router.use(require("./create-student/create-student"));
 router.use(require("./upload-subject/upload-subject"));
 router.use(require("./upload-class/upload-class"));
 router.use(require("./upload-class/upload-class_v1.2"));
