36dd319bdb54840001a1dd695428fe516b7fe66a
DinhHoangNam <dinhhoangnam162793@gmail.com>
2021-03-17 16:47:30 +0700

    :pencil2:  upload-class: use less redundancy schema version

--- routes/v1.2/staff/upload-class/helper.js
+++ routes/v1.2/staff/upload-class/helper.js
@@ -19,6 +19,31 @@ function parseExcel(rows) {
   return records;
 }
 
+function parseExcelV122(rows) {
+  // skip header
+  rows.shift();
+  // parse excel
+  const records = rows.map((row) => {
+    return {
+      semester: row[0].toString(),
+      classId: row[1].toString(),
+      subject: {
+        subjectId: row[2].toString(),
+        subjectName: row[3],
+        credit: row[4],
+      },
+      note: row[5],
+      teacherId: row[6],
+      studentIdsString: row[7],
+      studentIds: row[7]
+        .toString()
+        .split(",")
+        .map((sid) => sid.trim()),
+    };
+  });
+  return records;
+}
+
 async function getTeacherById(teacherId) {
   const teacherHistoryCol = (await connection).db().collection("TeacherHistory");
   const doc = await teacherHistoryCol.findOne({ "profiles.teacherId": teacherId }, { projection: { "profiles.$": 1, _id: 0 } });
@@ -27,14 +52,16 @@ async function getTeacherById(teacherId) {
 
 async function getStudentsByIds(studentIds) {
   const studentHistoryCol = (await connection).db().collection("StudentHistory");
-  const studentPromises = studentIds.map(async (studentId) => {
-    const doc = await studentHistoryCol.findOne(
-      { "profiles.studentId": studentId.toString() },
-      { projection: { "profiles.$": 1, _id: 0 } }
-    );
-    return doc ? doc.profiles[0] : null;
-  });
+  const studentPromises = studentIds
+    .filter((sid) => sid !== "")
+    .map(async (studentId) => {
+      const doc = await studentHistoryCol.findOne(
+        { "profiles.studentId": studentId.toString() },
+        { projection: { "profiles.$": 1, _id: 0 } }
+      );
+      return doc ? doc.profiles[0] : null;
+    });
   return Promise.all(studentPromises);
 }
 
-module.exports = { parseExcel, getTeacherById, getStudentsByIds };
+module.exports = { parseExcel, getTeacherById, getStudentsByIds, parseExcelV122 };
--- routes/v1.2/staff/upload-class/index.js
+++ routes/v1.2/staff/upload-class/index.js
@@ -11,8 +11,8 @@ const readXlsxFile = require("read-excel-file/node");
 const axios = require("axios").default;
 
 const { bufferToStream } = require("../utils");
-const { parseExcel, getTeacherById, getStudentsByIds } = require("./helper");
-const { mockupBKCResponse } = require("../../../utils");
+const { parseExcel, getTeacherById, getStudentsByIds, parseExcelV122 } = require("./helper");
+
 //
 router.get("/classes", authen, author(ROLE.STAFF), async (req, res) => {
   const classCol = (await connection).db().collection("Class");
@@ -20,43 +20,19 @@ router.get("/classes", authen, author(ROLE.STAFF), async (req, res) => {
   res.json(docs);
 });
 
-//
+// v1.2.2
 router.post("/upload-classes", authen, author(ROLE.STAFF), upload.single("excel-file"), async (req, res) => {
   try {
     const rows = await readXlsxFile(bufferToStream(req.file.buffer));
-    const records = parseExcel(rows);
-
-    // group studentIds by classId
-    const classId2StudentIds = records.reduce((accumulator, record) => {
-      accumulator[record.classId] = [...(accumulator[record.classId] || []), record.studentId];
-      return accumulator;
-    }, {});
-
-    // fill other info,
-    const simpleClasses = Object.entries(classId2StudentIds).map((entry) => {
-      const classInfo = records.find((record) => record.classId == entry[0]);
-      return {
-        semester: classInfo.semester,
-        classId: entry[0],
-        subject: {
-          subjectId: classInfo.subjectId,
-          subjectName: classInfo.subjectName,
-          credit: classInfo.credit,
-        },
-        note: classInfo.note,
-        teacherId: classInfo.teacherId,
-        studentIds: entry[1],
-      };
-    });
+    const records = parseExcelV122(rows);
 
     // pre-join students' info, teacher info,
-    const classesPromises = simpleClasses.map(async (claxx) => {
+    const classesPromises = records.map(async (claxx) => {
       // TODO: if not found item, --> res to FE to notif user
       claxx.teacher = await getTeacherById(claxx.teacherId);
       claxx.students = await getStudentsByIds(claxx.studentIds);
       return claxx;
     });
-
     let classes = await Promise.all(classesPromises);
 
     //
@@ -73,14 +49,13 @@ router.post("/upload-classes", authen, author(ROLE.STAFF), upload.single("excel-
         privateKeyHex: req.body.privateKeyHex,
         classes: payload,
       });
-      // const response = mockupBKCResponse(payload, "classId");
       classes.forEach((clx) => {
         clx.txid = response.data.transactions.find((tx) => tx.classId === clx.classId).transactionId;
       });
 
       const classCol = (await connection).db().collection("Class");
       const result = await classCol.insertMany(classes);
-      res.json(result.ops);
+      return res.json(result.ops);
     } catch (error) {
       console.error(error);
       if (error.response) return res.status(502).send(error.response.data);
@@ -91,5 +66,77 @@ router.post("/upload-classes", authen, author(ROLE.STAFF), upload.single("excel-
     return res.status(500).send(error.toString());
   }
 });
+
+// excel file too duplicate info
+// router.post("/upload-classes-v1.2.1", authen, author(ROLE.STAFF), upload.single("excel-file"), async (req, res) => {
+//   try {
+//     const rows = await readXlsxFile(bufferToStream(req.file.buffer));
+//     const records = parseExcel(rows);
+
+//     // group studentIds by classId
+//     const classId2StudentIds = records.reduce((accumulator, record) => {
+//       accumulator[record.classId] = [...(accumulator[record.classId] || []), record.studentId];
+//       return accumulator;
+//     }, {});
+
+//     // fill other info,
+//     const simpleClasses = Object.entries(classId2StudentIds).map((entry) => {
+//       const classInfo = records.find((record) => record.classId == entry[0]);
+//       return {
+//         semester: classInfo.semester,
+//         classId: entry[0],
+//         subject: {
+//           subjectId: classInfo.subjectId,
+//           subjectName: classInfo.subjectName,
+//           credit: classInfo.credit,
+//         },
+//         note: classInfo.note,
+//         teacherId: classInfo.teacherId,
+//         studentIds: entry[1],
+//       };
+//     });
+
+//     // pre-join students' info, teacher info,
+//     const classesPromises = simpleClasses.map(async (claxx) => {
+//       // TODO: if not found item, --> res to FE to notif user
+//       claxx.teacher = await getTeacherById(claxx.teacherId);
+//       claxx.students = await getStudentsByIds(claxx.studentIds);
+//       return claxx;
+//     });
+
+//     let classes = await Promise.all(classesPromises);
+
+//     //
+//     const payload = classes.map((cls) => ({
+//       classId: cls.classId,
+//       subjectId: cls.subject.subjectId,
+//       credit: cls.subject.credit,
+//       teacherPublicKey: cls.teacher.publicKey,
+//       studentPublicKeys: cls.students.map((std) => std.publicKey),
+//     }));
+
+//     try {
+//       const response = await axios.post("/staff/create-classes", {
+//         privateKeyHex: req.body.privateKeyHex,
+//         classes: payload,
+//       });
+//       // const response = mockupBKCResponse(payload, "classId");
+//       classes.forEach((clx) => {
+//         clx.txid = response.data.transactions.find((tx) => tx.classId === clx.classId).transactionId;
+//       });
+
+//       const classCol = (await connection).db().collection("Class");
+//       const result = await classCol.insertMany(classes);
+//       res.json(result.ops);
+//     } catch (error) {
+//       console.error(error);
+//       if (error.response) return res.status(502).send(error.response.data);
+//       return res.status(500).send(error.toString());
+//     }
+//   } catch (error) {
+//     console.error(error);
+//     return res.status(500).send(error.toString());
+//   }
+// });
 
 module.exports = router;
